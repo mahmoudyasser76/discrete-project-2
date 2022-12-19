746fac89edf8601bbb9faef55cebf5cd3fbf1fc5
Mahmoud Yasser <116122694+mahmoudyasser76@users.noreply.github.com>
2022-12-19 23:26:21 +0200

    Add files via upload

---
+++ perfect numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number range to get the perfect numbers between them:");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2= int.Parse(Console.ReadLine());
+            int i, j;
+            Console.WriteLine("perfect numbers between "+ num1 +" and "+ num2 +":");
+            for (i = num1; i <= num2; i++)
+            {
+                int sum= 0;
+                for (j = 1; j < i; j++)
+                {
+                    if ( i % j == 0)
+                    {
+                        sum+= j;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
